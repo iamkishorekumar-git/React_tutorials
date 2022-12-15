5ee43bfd4c809224479f2dfac1e92e786082ba1c
KlentyKishore <kishorekumar@klenty.com>
2022-12-15 08:18:09 +0530

    created jsx and added component

---
+++ jsx/src/App.js
@@ -0,0 +1,15 @@
+import ProfileCard from "./Components/ProfileCard";
+
+function App()
+{
+    return(
+        <div>
+            <div>Personal Digital Assitance</div>
+            <ProfileCard/>
+            <ProfileCard/>
+            <ProfileCard/>
+        </div>
+    ); 
+}
+
+export default App
---
+++ jsx/src/Components/ProfileCard.js
@@ -0,0 +1,6 @@
+function ProfileCard()
+{
+    return <div>ProfileCard</div>
+}
+
+export default ProfileCard
